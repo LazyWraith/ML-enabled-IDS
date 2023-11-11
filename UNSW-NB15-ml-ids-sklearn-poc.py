2b6df8f0c3ff48cadf38895a66e6a86ba0fa8af0
Weng Hong Lee <lee090102@gmail.com>
2023-11-12 02:20:35 +0800

    Minor correction

--- UNSW-NB15-ml-ids-sklearn-poc.py
+++ UNSW-NB15-ml-ids-sklearn-poc.py
@@ -109,18 +109,15 @@ def get_ts():
     return f"{ts:>08.4f}"
 
 printlog(f"[{get_ts()}] Init complete!")
-
 printlog(f"[{get_ts()}] Reading from {train_path}")
 
 # Read Train and Test dataset
 data_train = pd.read_csv(train_path)
 data_test = pd.read_csv(test_path)
 data_train.head()
-columns = (['id', 'dur', 'proto', 'service', 'state', 'spkts', 'dpkts', 'sbytes', 'dbytes', 'rate', 'sttl', 'dttl', 'sload', 'dload', 'sloss', 'dloss', 'sinpkt', 'dinpkt', 'sjit', 'djit', 'swin', 'stcpb', 'dtcpb', 'dwin', 'tcprtt', 'synack', 'ackdat', 'smean', 'dmean', 'trans_depth', 'response_body_len', 'ct_srv_src', 'ct_state_ttl', 'ct_dst_ltm', 'ct_src_dport_ltm', 'ct_dst_sport_ltm', 'ct_dst_src_ltm', 'is_ftp_login', 'ct_ftp_cmd', 'ct_flw_http_mthd', 'ct_src_ltm', 'ct_srv_dst', 'is_sm_ips_ports', 'attack_cat', 'label'])
 
 # Assign names for columns
-
-
+columns = (['id', 'dur', 'proto', 'service', 'state', 'spkts', 'dpkts', 'sbytes', 'dbytes', 'rate', 'sttl', 'dttl', 'sload', 'dload', 'sloss', 'dloss', 'sinpkt', 'dinpkt', 'sjit', 'djit', 'swin', 'stcpb', 'dtcpb', 'dwin', 'tcprtt', 'synack', 'ackdat', 'smean', 'dmean', 'trans_depth', 'response_body_len', 'ct_srv_src', 'ct_state_ttl', 'ct_dst_ltm', 'ct_src_dport_ltm', 'ct_dst_sport_ltm', 'ct_dst_src_ltm', 'is_ftp_login', 'ct_ftp_cmd', 'ct_flw_http_mthd', 'ct_src_ltm', 'ct_srv_dst', 'is_sm_ips_ports', 'attack_cat', 'label'])
 data_train.columns = columns
 data_test.columns = columns
 data_train.info()
@@ -134,7 +131,6 @@ data_test.loc[data_test['label'] == "Normal", "label"] = 0
 data_test.loc[data_test['label'] != 0, "label"] = 1
 
 def pie_plot(df, cols_list, rows, cols):
-    # print(f"[{get_ts()}] Generating results...", flush=True)
     fig, axes = plt.subplots(rows, cols)
     for ax, col in zip(axes.ravel(), cols_list):
         df[col].value_counts().plot(ax=ax, kind='pie', figsize=(15, 15), fontsize=10, autopct='%1.0f%%')
@@ -143,12 +139,6 @@ def pie_plot(df, cols_list, rows, cols):
     plt.savefig(os.path.join(output_dir, f"{counter}{col}_pie_chart.png"))  # Save the chart
     print(f"[{get_ts()}] Saved results to {output_dir}/{counter}{col}_pie_chart.png", flush=True)
     if (display_results): plt.show()
-
-def Scaling(df_num, cols):
-    std_scaler = RobustScaler()
-    std_scaler_temp = std_scaler.fit_transform(df_num)
-    std_df = pd.DataFrame(std_scaler_temp, columns=cols)
-    return std_df
 
 def preprocess(dataframe):
     df_num = dataframe.drop(cat_cols, axis=1)
@@ -162,7 +152,6 @@ def preprocess(dataframe):
     std_df = pd.DataFrame(std_scaler_temp, columns=num_cols)
     dataframe = pd.concat([std_df, labels], axis=1)
     return dataframe
-
 
 if (generate_statistics_pie):
     pie_plot(data_train, ['proto', 'service'], 1, 2)
@@ -189,12 +178,8 @@ pca_test = PCA(n_components=20)
 x_test_reduced = pca_test.fit_transform(x_test)
 y_test_reduced = y_test
 
-
-
-
 printlog(f"[{get_ts()}] Training set original features: {x_train.shape[1]}, reduced features: {x_train_reduced.shape[1]}")
 printlog(f"[{get_ts()}] Testing set original features: {x_test.shape[1]}, reduced features: {x_test_reduced.shape[1]}")
-
 
 kernal_evals = dict()
 def evaluate_classification(model, name, X_train, X_test, y_train, y_test):
@@ -257,6 +242,7 @@ if (bool_lr):
     end_time = time.time()
     printlog(f"[{get_ts()}] Training time: {(end_time - start_time):.4f}")
     evaluate_classification(lr, "Logistic Regression", x_train, x_test, y_train, y_test)
+
 if (bool_knn): 
     printlog(f"[{get_ts()}] Preparing KNeighborsClassifier")
     start_time = time.time()
@@ -264,6 +250,7 @@ if (bool_knn):
     end_time = time.time()
     printlog(f"[{get_ts()}] Training time: {(end_time - start_time):.4f}")
     evaluate_classification(knn, "KNeighborsClassifier", x_train, x_test, y_train, y_test)
+
 if (bool_gnb): 
     printlog(f"[{get_ts()}] Preparing GaussianNB")
     start_time = time.time()
@@ -271,6 +258,7 @@ if (bool_gnb):
     end_time = time.time()
     printlog(f"[{get_ts()}] Training time: {(end_time - start_time):.4f}")
     evaluate_classification(gnb, "GaussianNB", x_train, x_test, y_train, y_test)
+
 if (bool_lin_svc): 
     printlog(f"[{get_ts()}] Preparing Linear SVC(LBasedImpl)")
     start_time = time.time()
@@ -278,6 +266,7 @@ if (bool_lin_svc):
     end_time = time.time()
     printlog(f"[{get_ts()}] Training time: {(end_time - start_time):.4f}")
     evaluate_classification(lin_svc, "Linear SVC(LBasedImpl)", x_train, x_test, y_train, y_test)
+
 if (bool_dt): 
     printlog(f"[{get_ts()}] Preparing Decision Tree")
     start_time = time.time()
@@ -286,7 +275,6 @@ if (bool_dt):
     end_time = time.time()
     printlog(f"[{get_ts()}] Training time: {(end_time - start_time):.4f}")
     evaluate_classification(tdt, "DecisionTreeClassifier", x_train, x_test, y_train, y_test)
-
 
     features_names = data_train.drop(['label'], axis=1)
     f_importances(abs(tdt.feature_importances_), features_names, top=18, title="Decision Tree")
